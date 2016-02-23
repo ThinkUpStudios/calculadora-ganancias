c4b5716c6ce0ce854263f74d32a8031c18fe3002
dcamarro <dariocamarro@gmail.com>
2016-02-23 18:27:36 -0300

    Formateo numeros a dos decimales.

--- calculadora-ganancias/www/js/controllers.js
+++ calculadora-ganancias/www/js/controllers.js
@@ -14,11 +14,18 @@ angular.module('starter.controllers', [])
 
   $scope.calcular = function() {
      var resultados = CalculatorService.calcular($scope.sueldoBruto, $scope.tieneConyuge, $scope.cantidadHijos, $scope.familiaresACargo);
-     $scope.sueldoNeto = resultados.sueldoEnMano;
-     $scope.impustoPorGanancias = resultados.impuestoMensual;
-     $scope.descuentos = resultados.descuentosComunes;
-     $scope.descuentosTotales =resultados.descuentosComunes+resultados.impuestoMensual;
+     $scope.sueldoNeto = redondeo(resultados.sueldoEnMano,2);
+     $scope.impustoPorGanancias = redondeo(resultados.impuestoMensual,2);
+     $scope.descuentos = redondeo(resultados.descuentosComunes,2);
+     $scope.descuentosTotales =redondeo(resultados.descuentosComunes+resultados.impuestoMensual,2);
   };
+
+  function redondeo(numero, decimales)
+  {
+    var flotante = parseFloat(numero);
+    var resultado = Math.round(flotante*Math.pow(10,decimales))/Math.pow(10,decimales);
+    return resultado;
+  }
 })
 
 
