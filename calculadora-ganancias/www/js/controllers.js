594688db239e5252be4d04b58b7aa2f27d8a30bc
Facundo Costa Zini <fcostazini@conexia.com>
2016-02-26 14:36:52 -0300

    prueba de admob

--- calculadora-ganancias/www/js/controllers.js
+++ calculadora-ganancias/www/js/controllers.js
@@ -26,6 +26,66 @@ angular.module('starter.controllers', [])
     var resultado = Math.round(flotante*Math.pow(10,decimales))/Math.pow(10,decimales);
     return resultado;
   }
+  
+  
+function initAds() {
+      if (admob) {
+        var adPublisherIds = {
+    //      ios : {
+      //      banner : "ca-app-pub-XXXXXXXXXXXXXXXX/BBBBBBBBBB"
+        //  },
+          android : {
+            banner : "ca-app-pub-9559909155606690/4831654369"
+          }
+        };
+
+        var admobid = (/(android)/i.test(navigator.userAgent)) ? adPublisherIds.android : adPublisherIds.ios;
+
+        admob.setOptions({
+          publisherId:      admobid.banner,          
+          tappxShare:       0.5
+        });
+
+        registerAdEvents();
+
+      } else {
+        alert('AdMobAds plugin not ready');
+      }
+    }
+
+    function onAdLoaded(e) {
+      if (e.adType === admob.AD_TYPE.INTERSTITIAL) {
+        admob.showInterstitialAd();
+        showNextInterstitial = setTimeout(function() {
+          admob.requestInterstitialAd();
+        }, 2 * 60 * 1000); // 2 minutes
+      }
+    }
+
+    // optional, in case respond to events
+    function registerAdEvents() {
+      document.addEventListener(admob.events.onAdLoaded, onAdLoaded);
+      document.addEventListener(admob.events.onAdFailedToLoad, function (e) {});
+      document.addEventListener(admob.events.onAdOpened, function (e) {});
+      document.addEventListener(admob.events.onAdClosed, function (e) {});
+      document.addEventListener(admob.events.onAdLeftApplication, function (e) {});
+      document.addEventListener(admob.events.onInAppPurchaseRequested, function (e) {});
+    }
+
+    function onDeviceReady() {
+      document.removeEventListener('deviceready', onDeviceReady, false);
+      initAds();
+
+      // display a banner at startup
+      admob.createBannerView();
+
+      // request an interstitial
+      admob.requestInterstitialAd();
+    }
+
+    document.addEventListener("deviceready", onDeviceReady, false);
+
 })
+
 
 
