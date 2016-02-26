21be851c2ff63af5180fbfe0c7a63555b16fc332
Facundo Costa Zini <fcostazini@conexia.com>
2016-02-26 15:51:59 -0300

    fix estetico

--- calculadora-ganancias/www/js/controllers.js
+++ calculadora-ganancias/www/js/controllers.js
@@ -19,71 +19,31 @@ angular.module('starter.controllers', [])
      $scope.descuentos = redondeo(resultados.descuentosComunes,2);
      $scope.descuentosTotales =redondeo(resultados.descuentosComunes+resultados.impuestoMensual,2);
   };
+  $scope.modificarCantidadHijos = function (valor) {
+	$scope.cantidadHijos += valor;
+	if ($scope.cantidadHijos < 0) {
+		$scope.cantidadHijos = 0;
+		$scope.calcular();
+	}
 
+  };
+  $scope.modificarFamiliaresACargo = function (valor) {
+	$scope.familiaresACargo += valor;
+	if ($scope.familiaresACargo < 0) {
+		$scope.familiaresACargo = 0;
+		$scope.calcular();
+		
+	}
+
+  };  
+  
+	
   function redondeo(numero, decimales)
   {
     var flotante = parseFloat(numero);
     var resultado = Math.round(flotante*Math.pow(10,decimales))/Math.pow(10,decimales);
     return resultado;
   }
-  
-  
-function initAds() {
-      if (admob) {
-        var adPublisherIds = {
-    //      ios : {
-      //      banner : "ca-app-pub-XXXXXXXXXXXXXXXX/BBBBBBBBBB"
-        //  },
-          android : {
-            banner : "ca-app-pub-9559909155606690/4831654369"
-          }
-        };
-
-        var admobid = (/(android)/i.test(navigator.userAgent)) ? adPublisherIds.android : adPublisherIds.ios;
-
-        admob.setOptions({
-          publisherId:      admobid.banner,          
-          tappxShare:       0.5
-        });
-
-        registerAdEvents();
-
-      } else {
-        alert('AdMobAds plugin not ready');
-      }
-    }
-
-    function onAdLoaded(e) {
-      if (e.adType === admob.AD_TYPE.INTERSTITIAL) {
-        admob.showInterstitialAd();
-        showNextInterstitial = setTimeout(function() {
-          admob.requestInterstitialAd();
-        }, 2 * 60 * 1000); // 2 minutes
-      }
-    }
-
-    // optional, in case respond to events
-    function registerAdEvents() {
-      document.addEventListener(admob.events.onAdLoaded, onAdLoaded);
-      document.addEventListener(admob.events.onAdFailedToLoad, function (e) {});
-      document.addEventListener(admob.events.onAdOpened, function (e) {});
-      document.addEventListener(admob.events.onAdClosed, function (e) {});
-      document.addEventListener(admob.events.onAdLeftApplication, function (e) {});
-      document.addEventListener(admob.events.onInAppPurchaseRequested, function (e) {});
-    }
-
-    function onDeviceReady() {
-      document.removeEventListener('deviceready', onDeviceReady, false);
-      initAds();
-
-      // display a banner at startup
-      admob.createBannerView();
-
-      // request an interstitial
-      admob.requestInterstitialAd();
-    }
-
-    document.addEventListener("deviceready", onDeviceReady, false);
 
 })
 
