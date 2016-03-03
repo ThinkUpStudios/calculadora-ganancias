98df51516cdba0aeb5f170832a97f44a9ed16f57
FaQ <fcostazini@gmail.com>
2016-03-03 09:21:19 -0300

    agrego ionic material

--- calculadora-ganancias/www/js/app.js
+++ calculadora-ganancias/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic','starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic','ionic-material','starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -31,6 +31,8 @@ angular.module('starter', ['ionic','starter.controllers', 'starter.services'])
           if (focusedElement != this) {
             this.select();
             focusedElement = this;
+          }else{
+            this.select(false);
           }
         });
         element.on('blur', function () {
